804ce7fda14cb9c9f7e70897a12d1e8860a318b3
AMV1909 <amorales264@unab.edu.co>
2023-02-09 18:34:50 -0500

    flutter-first-app

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My First App'),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Image.asset('images/the_knight.webp'),
+      ),
+    );
+  }
+}
